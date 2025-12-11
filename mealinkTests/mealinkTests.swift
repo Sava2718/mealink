b898199579a7c9f083c628ced21f69058461c3a8
MIKI NAWATA <miki0220musashihappy@icloud.com>
2025-12-11 20:10:47 +0900

    Initial Commit

---
+++ mealinkTests/mealinkTests.swift
@@ -0,0 +1,17 @@
+//
+//  mealinkTests.swift
+//  mealinkTests
+//
+//  Created by Tya NaWa on 2025/12/11.
+//
+
+import Testing
+@testable import mealink
+
+struct mealinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
